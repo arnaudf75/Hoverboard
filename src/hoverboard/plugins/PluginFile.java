0fcfdbb6a62f89c7b6b921bdb33c56ee7894686e
Amram Elbaz <elbaz.amram@gmail.com>
2015-04-26 16:36:10 +0200

    Ajout classe PluginFile
    Classe chargée de localiser les fichiers plugins, utile à l'ajout et
    suppression de plugins en local (hors activation/ désactivation).

---
+++ src/hoverboard/plugins/PluginFile.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoverboard.plugins;
+
+/**
+ *
+ * @author Amram
+ */
+public class PluginFile {
+    private String pathOnMachine;
+
+    /**
+     * @return the pathOnMachine
+     */
+    public String getPathOnMachine() {
+        return pathOnMachine;
+    }
+
+    /**
+     * @param pathOnMachine the pathOnMachine to set
+     */
+    public void setPathOnMachine(String pathOnMachine) {
+        this.pathOnMachine = pathOnMachine;
+    }
+    
+    
+}
